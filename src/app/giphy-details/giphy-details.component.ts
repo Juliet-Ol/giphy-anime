89a921f76eb32c5bcefca3e91a237da7e9e66711
Juliet <agutu@gmail.com>
2022-02-11 22:22:00 +0300

    style the search button

--- src/app/giphy-details/giphy-details.component.ts
+++ src/app/giphy-details/giphy-details.component.ts
@@ -11,5 +11,6 @@ export class GiphyDetailsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
 
 }
--- src/app/giphy-service/giphy.service.ts
+++ src/app/giphy-service/giphy.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GiphyService {
 
-  constructor(private http:HttpClient) { }
+   
+
+  constructor(private http:HttpClient) { 
+    this.http = http;
+  }
+
+  getTrendingGifs() {
+    return this.http.get(`http://api.giphy.com/v1/gifs/trending?api_key=${environment.giphyApiKey}&limit=50`);
+
+  }
+
+  searchGifs(search:string) {
+    return this.http.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${environment.giphyApiKey}&limit=50`);
+
+  }
 }
+
+
