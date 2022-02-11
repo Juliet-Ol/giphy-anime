baae2cbf319e2347dcf88aa35b3d591e766a4a49
Juliet <agutu@gmail.com>
2022-02-11 18:25:48 +0300

    add giphy key generated from API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  giphyApiKey: 'ldCzQQyAx2tN1PabfEOaqljKSi7Q3CCo'
 };
 
 /*
